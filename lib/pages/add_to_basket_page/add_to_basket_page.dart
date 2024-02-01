9122c3f3b4a444e817af5fdc435fba5c7b1ceb35
WeThinkCode Bootcamper <llebelwane023@student.wethinkcode.co.za>
2024-02-01 17:43:39 +0200

    add to basket page structure

---
+++ lib/pages/add_to_basket_page/add_to_basket_page.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class AddToBasketPage extends StatefulWidget {
+  const AddToBasketPage({super.key});
+
+  @override
+  State<AddToBasketPage> createState() => _AddToBasketPageState();
+}
+
+class _AddToBasketPageState extends State<AddToBasketPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [],
+      ),
+    );
+  }
+}
